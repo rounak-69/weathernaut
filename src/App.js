49fbdb561e717a1dc547ab403c0c5eb7cffabf3e
rounakdev-333 <122973121+rounak-69@users.noreply.github.com>
2024-08-05 17:57:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="">
+          Download Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="">
+          Rounak Jhajharia
+        </a>{" "}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "Your API Key here Generated From openweather",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
